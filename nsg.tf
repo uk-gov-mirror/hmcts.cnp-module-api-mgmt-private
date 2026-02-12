c74c39912224151414e2b990bf4320cba1b9f3b8
Alex Bance <Alex.Bance@justice.gov.uk>
2026-02-12 10:21:13 +0000

    feat: support custom NSG rules

--- nsg.tf
+++ nsg.tf
@@ -107,3 +107,24 @@ resource "azurerm_network_security_rule" "deny" {
   resource_group_name         = var.virtual_network_resource_group
   network_security_group_name = azurerm_network_security_group.apim.name
 }
+
+resource "azurerm_network_security_rule" "custom" {
+  for_each = var.custom_nsg_rules
+
+  name                         = each.key
+  priority                     = each.value.priority
+  direction                    = each.value.direction
+  access                       = each.value.access
+  protocol                     = each.value.protocol
+  source_port_range            = each.value.source_port_range
+  source_port_ranges           = each.value.source_port_ranges
+  destination_port_range       = each.value.destination_port_range
+  destination_port_ranges      = each.value.destination_port_ranges
+  source_address_prefix        = each.value.source_address_prefix
+  source_address_prefixes      = each.value.source_address_prefixes
+  destination_address_prefix   = each.value.destination_address_prefix
+  destination_address_prefixes = each.value.destination_address_prefixes
+  description                  = each.value.description
+  resource_group_name          = var.virtual_network_resource_group
+  network_security_group_name  = azurerm_network_security_group.apim.name
+}
